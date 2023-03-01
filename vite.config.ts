496258e8503aaec0e72602011af2e53a33895910
Anthony Olivence <anthony.olivence@gmail.com>
2023-03-01 16:52:46 -0500

    (chore): Add eslint plugin for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 /* eslint-disable import/namespace */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), eslintPlugin({ cache: false, failOnError: false })],
 });
